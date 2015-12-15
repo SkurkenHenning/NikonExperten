408b73ca65b06b4c2dc6bc5d1ad712fb7147dc04
Carsten Nielsen <carsten.beck.nielsen@hotmail.com>
2015-12-15 10:51:14 +0100

    Added Models

---
+++ RepoNE/Models/Forside.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoNE
+{
+    public class Forside
+    {
+        public int ID { get; set; }
+        public string Nav { get; set; }
+        public string SliderBilleder { get; set; }
+    }
+}
---
+++ RepoNE/Models/Kontakt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoNE
+{
+    public class Kontakt
+    {
+        public int ID { get; set; }
+        public string Overskrift { get; set; }
+        public string Adresse { get; set; }
+        public int PostNr { get; set; }
+        public int Tlf { get; set; }
+        public int Fax { get; set; }
+        public string Email { get; set; }
+        public string HjemmesideLink { get; set; }
+        public string Aabningstider { get; set; }
+        public string ByNavn { get; set; }
+    }
+}
