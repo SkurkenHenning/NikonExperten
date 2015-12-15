a2c0da9e83457f1e971c5c0a53163a16211e75fb
Andreas Visby <andreasvisby@gmail.com>
2015-12-15 10:22:50 +0100

    Ny fil og rettellse på opretProdukt

---
+++ RepoNE/Models/Viewmodels/KategoriProduktliste.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoNE.Models.Viewmodels
+{
+    public class KategoriProduktliste
+    {
+        public List<Kategori> listKategori { get; set; }
+        public List<Produkt> listProdukt { get; set; }  
+    }
+}
