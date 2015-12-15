6cad5c0b0bbc4922c6c657d3da116f2447582aff
Andreas Visby <andreasvisby@gmail.com>
2015-12-15 12:58:37 +0100

    Rediger af ting og viewmodel

--- NikonExperten/Areas/Admin/Controllers/AdminController.cs
+++ NikonExperten/Areas/Admin/Controllers/AdminController.cs
@@ -73,5 +73,54 @@ namespace NikonExperten.Areas.Admin.Controllers
             }
             return View("Index");
         }
+
+
+        public ActionResult SletRedigerKategori()
+        {
+            return View(kf.GetAll());
+        }
+
+        public ActionResult SletRedigerKategoriResult(int id)
+        {
+            kf.Delete(id);
+            return View("SletRedigerKategori", kf.GetAll());
+        }
+
+        public ActionResult RedigerKategori(int id)
+        {
+            return View(kf.Get(id));
+        }
+        [HttpPost]
+        public ActionResult RedigerKategori(Kategori k, HttpPostedFileBase Fil)
+        {
+            if (Fil != null)
+            {
+                Uploader u = new Uploader();
+                string path = Path.GetFullPath(Request.PhysicalApplicationPath + "Content/images/kategorier/");
+                string file = u.UploadImage(Fil, path, 200, true);
+
+                k.Billede = Path.GetFileName(file);
+                kf.Update(k);
+                @ViewBag.MSG = "Produktet er oprettet med det valgte billede";
+            }
+            else
+            {
+                kf.Update(k);
+                @ViewBag.MSG = "Produktet er oprettet med standard billede";
+            }
+            return View("SletRedigerKategori", kf.GetAll());
+        }
+
+        public ActionResult RedigerProdukt(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RedigerProdukt()
+        {
+            return View(pf.Get(id));
+        }
+
+
     }
 }
---
+++ RepoNE/Models/Viewmodels/ProduktListe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoNE.Models.Viewmodels
+{
+    public class ProduktListe
+    {
+        public Kategori Kategori { get; set; }
+        public List<Produkt> listProdukt { get; set; } 
+    }
+}
